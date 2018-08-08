99b994c3cc9b77bfa60165a93becaed2a83cfd5a
orchid00 <orchid00@gmail.com>
2018-08-08 16:27:26 +0200

    Add code about dplyr

--- scripts/002_plots.R
+++ scripts/002_plots.R
@@ -116,23 +116,92 @@ countriesByLetter(fl = c("A", "C" ))
 countries_A_C <- countriesByLetter(fl = c("A", "C" ))
 View(countries_A_C)
 
-countriesByLetter <- function(fl){
+# Function plot by country ----
+plotCountriesByLetter <- function(fl){
     my_countries <- gapminder[starts_with %in% fl, ]
-    my_countries
-    return(my_countries)
+    ggplot(data = my_countries, aes( x = year, y = lifeExp,
+                                     colour = continent)) +
+        geom_line() +
+        facet_wrap( ~ country)
+    
+    names <- paste(fl, collapse = "_") 
+    ggsave(filename = paste0("figs/LifeExp_countries_", 
+                             names, ".png"))
 }
 
+plotCountriesByLetter(c("D", "I"))
 
 
+# Save data ----
+my_countries
+View(my_countries)
+
+write_csv(x = my_countries, 
+          path = "data/countriesR_Y.csv" )
 
 
+# Data manipulation with dplyr ----
+?`dplyr-package`
+?`%>%` # ctrl+Shift+M
+
+head(gapminder)
+
+gapminder %>% 
+    head()
+
+# What is the number of unique countries
+# 142
+
+gapminder$country %>% 
+    unique() %>% 
+    length()
+
+# select() ----
+# picks variables based on their names
+# It only affects columns and keeps all rows 
+# Using select() selectry country and continent 
+# from the gapminder
+
+gapminder %>% 
+    select(country, continent)
+
+select(gapminder, country, continent)
+
+# Select those columns that starts_with the letter c
+# from the gapminder
+
+gapminder %>%
+    select(starts_with("c"))
+
+select(gapminder, starts_with("c"))
 
 
+# filter() ----
+# picks cases based on their values
+gapminder %>% 
+    filter(year == "2007")
 
+gapminder %>% 
+    filter(year > 2000)
 
+# Select your country and the year 2002
+# from the gapminder
+# Tip two columns one row 
+gapminder %>% 
+    select(country, year, pop) %>% 
+    filter(country == "Italy", year == 2002)
 
+# Select African countries for
+# lifeExp, country and year, 
+# How many rows does your data
+# have and why?
 
+gapminder %>% 
+    filter(continent == "Africa" ) %>% 
+    select(year, country, lifeExp)
 
-
+# Save this into african_countries
+dim(african_countries)
+# write african_countries as a csv file in data/
 
 
